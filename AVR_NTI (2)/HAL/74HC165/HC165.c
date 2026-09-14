ddb0c5ff5ef1ae7fccde7cb853507b35c52d0fa4
eman elsayed <eman.elsayed.ali9@gmail.com>
2026-09-14 19:47:55 +0300

    Fix error handling in SPI_InitMaster function

---
+++ AVR_NTI (2)/HAL/74HC165/HC165.c
@@ -0,0 +1,12 @@
+/*
+ * Author: Eman Elsayed Ali
+ * Email:  eman.elsayed.ali9@gmail.com
+ */
+
+  // #include <util/delay.h>
+ 
+#include "STD_TYPES.h"
+#include "MATH.h"
+#include "GPIO_INTERFACE.h"
+#include "SPI_INTERFACE.h"
+#include "HC165_INTERFACE.h"
