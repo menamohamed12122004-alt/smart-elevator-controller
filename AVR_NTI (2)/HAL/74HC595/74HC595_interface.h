8a735ff158fd5d9fdbb2ea6d47368b817fc3c07b
Mariam Salah <101mariamsalah@gmail.com>
2026-09-15 15:44:13 +0300

    Enhance 74HC595 driver: add display floor function, improve initialization, and update send byte logic

--- AVR_NTI (2)/HAL/74HC595/74HC595_interface.h
+++ AVR_NTI (2)/HAL/74HC595/74HC595_interface.h
@@ -3,8 +3,15 @@
 
 #include "STD_TYPES.h"
 
+
+#define HC595_LATCH_PORT    DIO_PORTC   // أو PORTC حسب درايفر الـ GPIO عندك
+#define HC595_LATCH_PIN     DIO_PIN3    // أو PIN3
+
+// الدوال الأساسية
 STD_ReturnType HC595_Init(void);
+STD_ReturnType HC595_SendByte(uint8_t Copy_u8Data);
 
-STD_ReturnType HC595_SendByte(uint8 Copy_u8Data);
+// دالة إضافية لعرض رقم الدور مباشرة على الـ 7-Segment
+STD_ReturnType HC595_DisplayFloor(uint8_t Copy_u8FloorNum);
 
-#endif
+#endif 
