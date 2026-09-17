71a4468ede8c02f519f6ea68f7aea6821f5eaf21
asmaa <salamaasma05@gmail.com>
2026-09-17 19:39:45 +0300

    Update DOOR_PORT definition to use GPIO_PORTA

--- AVR_NTI (2)/HAL/door/door_private.h
+++ AVR_NTI (2)/HAL/door/door_private.h
@@ -1,7 +1,7 @@
 #ifndef DOOR_CTRL_PRIV_H_
 #define DOOR_CTRL_PRIV_H_
 
-#define DOOR_PORT             GPIO_PORTB
+#define DOOR_PORT             GPIO_PORTA
 #define DOOR_PIN_OPEN         GPIO_PIN0
 #define DOOR_PIN_CLOSE        GPIO_PIN1
 
