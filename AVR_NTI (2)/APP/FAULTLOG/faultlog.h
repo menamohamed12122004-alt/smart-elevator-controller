3e4411d3abe6c5a93b98adf5d9ab9ac7ccf48f5b
Menam Mohamed <menamohamed12122004-alt@users.noreply.github.com>
2026-09-17 12:27:54 +0300

    fault

--- AVR_NTI (2)/APP/FAULTLOG/faultlog.h
+++ AVR_NTI (2)/APP/FAULTLOG/faultlog.h
@@ -1,9 +1,10 @@
+#ifndef FAULTLOG_H
 #define FAULTLOG_H
 
 #include "STD_TYPES.h"
 #include "safety.h"
 
-#define FAULT_LOG_SIZE    16
+#define FAULT_LOG_SIZE 16
 
 typedef struct {
     Safety_Fault_t faultCode;
@@ -12,14 +13,14 @@ typedef struct {
     uint32 timeStamp;
 } FaultRecord_t;
 
-/* ---------------- Function Prototypes ---------------- */
-
 void FLG_Init(void);
 
-/* Append a fault snapshot to the 16-entry Ring Buffer */
-void FLG_Append(Safety_Fault_t Copy_eFault, uint8 Copy_u8Floor, uint8 Copy_u8DoorState, uint32 Copy_u32Time);
+void FLG_Append(
+    Safety_Fault_t Copy_eFault,
+    uint8 Copy_u8Floor,
+    uint8 Copy_u8DoorState,   uint32 Copy_u32Time
+);
 
-/* Dump all stored faults over UART (Building Console) */
 void FLG_Dump(void);
 
 #endif /* FAULTLOG_H */
